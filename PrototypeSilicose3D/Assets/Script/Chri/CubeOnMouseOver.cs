74bd134266c89b25e6895a0ebab10ea71f57517e
Christian Lavoie <cristianlavoie111@gmail.com>
2016-05-24 18:08:48 -0400

    changement de couleur avec hover

---
+++ PrototypeSilicose3D/Assets/Script/Chri/CubeOnMouseOver.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeOnMouseOver : MonoBehaviour {
+
+    [SerializeField] Material Over;
+    [SerializeField] Material Aways;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver() {
+
+        GetComponent<Renderer>().material = Over;
+
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material = Aways;    
+    }
+}
